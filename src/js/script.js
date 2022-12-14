1c549ebb7f71c93dcbab1bd966a2099ac98120db
Tyrcnex <tyrcnex@gmail.com>
2022-12-14 18:49:21 +0800

    massive css organization + popup bug in js
    popup bug - if you click a button in the navbar and then click another button in the navbar after, the previous popup wouldnt hide. this causes a problem when you go through all the popups but dont want to click the cancel button

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 let navBar = document.getElementById('navBar');
 let navButtons = document.getElementsByClassName('item');
 let cancelButton = document.getElementById('cancel');
+let popUpSection = document.getElementById('popups');
 
 setTimeout(() => {
     document.getElementById('load').classList.add('hide');
@@ -12,22 +13,24 @@ setTimeout(() => {
 
 for (var i = 0; i < navButtons.length; i++) {
     navButtons[i].onclick = function() {
-        let preview = document.getElementById(`preview-${this.getElementsByTagName('*')[0].innerHTML.toLowerCase()}`)
-        preview.classList.add('show');
+        let previews = [...document.getElementsByClassName('popUpShow')];
+        let thisPreviewID = `preview-${this.getElementsByTagName('p')[0].innerHTML.toLowerCase()}`;
+        previews.filter(e => e.id !== thisPreviewID).forEach(e => e.classList.remove('popUpShow'));
+        document.getElementById(thisPreviewID).classList.add('popUpShow');
+
         cancelButton.style.display = 'block';
         document.documentElement.style.overflowY = 'hidden';
-        document.getElementById('popups').style.zIndex = '100';
+        popUpSection.style.zIndex = '100';
     }
 }
 
 
 cancelButton.onclick = function() {
     cancelButton.style.display = 'none';
-    let previews = Array.from(document.getElementsByClassName('preview'));
+    let previews = Array.from(document.getElementsByClassName('popup'));
     previews.forEach(e => {
-        if (e.classList.contains('show')) e.classList.remove('show');
+        if (e.classList.contains('popUpShow')) e.classList.remove('popUpShow');
     });
     document.documentElement.style.overflowY = 'scroll';
-    document.getElementById('popups').style.zIndex = '-5';
+    popUpSection.style.zIndex = '-5';
 }
-
